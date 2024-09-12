bc2d8ed7f10bf7a9a7c1eefceff54a62d071870e
mygithubshivam <kumarshivam3127@gmail.com>
2024-09-12 23:36:15 +0530

    added hero navbar sidebar parallax components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import "./app.scss";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
   return (
     <div>
-      <section>Hero</section>
-      <section>Parallax</section>
+      <section id="Home">
+        <Navbar />
+        <Hero />
+      </section>
+
+      <section id="Services">
+        {" "}
+        <Parallax type="services" />
+      </section>
       <section>Services</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
-      <section>Contact</section>
+      <section id="Contact">Contact</section>
     </div>
   );
 };
